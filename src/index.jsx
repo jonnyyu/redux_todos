3f0a5ef2ee355e98257b1572ea8e098a8d13342d
Jonny Yu <yingshen.yu@gmail.com>
2016-08-23 23:46:09 +0800

    basic redux works

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,20 @@
-console.log("Hello World");
+import todoApp from './reducers.js';
+import { createStore } from 'redux';
+
+import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } from './actions.js';
+
+let store = createStore(todoApp);
+
+console.log(store.getState());
+
+store.dispatch(addTodo("Hello Todo"));
+
+console.log(store.getState());
+
+store.dispatch(toggleTodo(0));
+
+console.log(store.getState());
+
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_ACTIVE));
+
+console.log(store.getState());
